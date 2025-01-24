b1cd3ed85de453d4195b7dd12dc9f25bf47c908d
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-24 16:42:52 +0100

    feat: ADD navbar on header

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route element={<AppLayout />}>
             <Route path="/" element={<HomePage />} />
-            <Route path="/movies" element={<MoviesPage />}/>
+            <Route path="/movies" element={<MoviesPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
