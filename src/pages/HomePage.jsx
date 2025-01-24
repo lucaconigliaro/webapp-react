2d1689a0ed45562d5dc2d6c4417107bd7e832ce2
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-24 17:01:10 +0100

    update: ADD show films button on the HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Homepage() {
     return (
         <>
             <section>
                 <h1>HomePage</h1>
+                <Link to="/movies" className="btn btn-primary">
+                    Show Movies
+                </Link>
             </section>
         </>
     )
