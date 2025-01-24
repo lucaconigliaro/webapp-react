2d4555848a91da1ee1ae72f42cd711b1d4c55840
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-24 16:08:22 +0100

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+function Homepage() {
+    return (
+        <>
+            <section>
+                <h1>HomePage</h1>
+            </section>
+        </>
+    )
+};
+
+export default Homepage;
