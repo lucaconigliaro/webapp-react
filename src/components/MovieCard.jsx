0775e162a2f7fd14ef9d2f9a9a184a8201984d3b
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-24 18:26:06 +0100

    update: changed the card to have the same size

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,19 +2,21 @@ function MovieCard({ movie }) {
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     
     return (
-        <>
-            <div className="card h-100">
-                <img src={`${backendUrl}/images/${movie.image}`} className="card-img-top" alt={movie.title} />
-                <div className="card-body">
-                    <h5 className="card-title">
-                        {movie.title} <br /> {movie.director}
-                    </h5>
-                    <p className="card-text">S{movie.abstract}.</p>
-                    <a href="#" className="btn btn-primary">Show details</a>
-                </div>
+        <div className="card h-100 d-flex flex-column">
+            <img 
+                src={`${backendUrl}/images/${movie.image}`} 
+                className="card-img-top" 
+                alt={movie.title} 
+            />
+            <div className="card-body d-flex flex-column">
+                <h5 className="card-title">
+                    {movie.title} <br /> {movie.director}
+                </h5>
+                <p className="card-text">{movie.abstract}.</p>
+                <a href="#" className="btn btn-primary mt-auto">Show details</a>
             </div>
-        </>
-    )
+        </div>
+    );
 };
 
 export default MovieCard;
