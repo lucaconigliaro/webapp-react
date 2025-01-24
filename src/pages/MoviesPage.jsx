a10e3994c9f6b6290c4be7fa4bffe3d853748a45
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-24 18:27:06 +0100

    update: ADD filters on movies page

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -4,29 +4,57 @@ import MovieCard from "../components/MovieCard";
 
 function MoviesPage() {
     const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState("");
+
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-        useEffect(() => {
-            axios.get(`${backendUrl}/movies`).then((resp) => {
-                setMovies(resp.data.data);
-            });
-        }, [])
+    const getMovies = () => {
+        const params = {};
+        if (search.length > 0) {
+            params.search = search;
+        }
+        axios.get(`${backendUrl}/movies`, { params }).then((resp) => {
+            setMovies(resp.data.data);
+        });
+    };
 
+    useEffect(() => {
+        getMovies()
+    }, []);
+
+    
     return (
         <>
             <section>
-                <h1>Movies Page</h1>
-                <p>Tutti i film nel catalogo</p>
-            </section>
-            <section>
-                <h2>Movies List</h2>
-                <div className="row row-cols-1 row-cols-sm-2 row cols-md-3 row-cols-lg-4 g-3">
-                    {movies.map((curMovie) => (
-                        (<div className="col-12 col-sm-6 col-md-4 col-lg-3" key={curMovie.id}>
-                            <MovieCard movie={curMovie} />
-                        </div>)
-                    ))}
+                <h1>Movies List</h1>
+                <div className="my-4 d-flex">
+                    <input
+                        value={search}
+                        onChange={(event) => setSearch(event.target.value)}
+                        type="search"
+                        className="form-control"
+                        placeholder="Search Movie"
+                        aria-label="Search Movie"
+                    />
+                    <button
+                        onClick={getMovies}
+                        className="btn btn-primary ms-2">
+                        Search
+                    </button>
                 </div>
+                {movies.length > 0 ? (
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
+                        {movies.map((curMovie) => (
+                            <div className="col" key={curMovie.id}>
+                                <MovieCard movie={curMovie} />
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="alert alert-warning">
+                        Movies Not Found
+                    </div>
+                )}
             </section>
         </>
     )
