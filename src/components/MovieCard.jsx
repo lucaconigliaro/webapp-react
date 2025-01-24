66a27056900b3d33fbbf987360416f4bc7fc4edd
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-24 17:59:55 +0100

    feat: ADD move card with global css

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,20 @@
+function MovieCard({ movie }) {
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    
+    return (
+        <>
+            <div className="card h-100">
+                <img src={`${backendUrl}/images/${movie.image}`} className="card-img-top" alt={movie.title} />
+                <div className="card-body">
+                    <h5 className="card-title">
+                        {movie.title} <br /> {movie.director}
+                    </h5>
+                    <p className="card-text">S{movie.abstract}.</p>
+                    <a href="#" className="btn btn-primary">Show details</a>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default MovieCard;
