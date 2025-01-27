49433805eeebd349d5de98d3e11b684d6c3a9c62
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-27 13:16:44 +0100

    update: ADD link for the single movie

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,23 @@
+import { Link } from "react-router-dom";
+
 function MovieCard({ movie }) {
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
-    
+
     return (
         <div className="card h-100 d-flex flex-column">
-            <img 
-                src={`${backendUrl}/images/${movie.image}`} 
-                className="card-img-top" 
-                alt={movie.title} 
+            <img
+                src={`${backendUrl}/images/${movie.image}`}
+                className="card-img-top"
+                alt={movie.title}
             />
             <div className="card-body d-flex flex-column">
                 <h5 className="card-title">
                     {movie.title} <br /> {movie.director}
                 </h5>
                 <p className="card-text">{movie.abstract}.</p>
-                <a href="#" className="btn btn-primary mt-auto">Show details</a>
+                <Link className="btn btn-primary mt-auto" to={`/movies/${movie.id}`}>
+                    Show details
+                </Link>
             </div>
         </div>
     );
