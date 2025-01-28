ef06f7e3fa735899162de540707c5c6bc9521ef7
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-28 15:50:32 +0100

    feat: ADD form validation and error handling

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,25 +1,41 @@
 import { useState } from "react";
 
-const initialValues = {
-    name: "",
-    text: "",
-    vote: 0,
-};
-
-function ReviewForm() {
+function ReviewForm({ formData, setFormData, submitReview }) {
     const votes = Array.from(Array(6).keys());
-    const [formData, setFormData] = useState(initialValues);
-    
+    const [error, setError] = useState(false);
+
     const setFieldValue = (event) => {
         const value = event.target.value;
         const fieldName = event.target.name;
         const newFormData = { ...formData };
         newFormData[fieldName] = value;
         setFormData(newFormData);
-    }
+    };
+
+    const isDataValid = () => {
+        if (
+            formData.name.length <= 3 ||
+            formData.vote < 0 ||
+            formData.vote > 5 ||
+            (formData.text.length > 0 && formData.text.length < 5)
+        ) {
+            return false;
+        }
+        return true;
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setError(false);
+        if (!isDataValid()) {
+            setError(true);
+        } else {
+            submitReview(formData);
+        }
+    };
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="username" className="form-label">
                     Name:
@@ -34,8 +50,7 @@ function ReviewForm() {
                     value={formData.name}
                 />
             </div>
-            <div
-                className="mb-3 w-50">
+            <div className="mb-3 w-50">
                 <label htmlFor="text" className="form-label">
                     Leave a review:
                 </label>
@@ -48,9 +63,7 @@ function ReviewForm() {
                     value={formData.text}></textarea>
             </div>
             <div className="mb-3 w-25">
-                <label htmlFor="vote">
-                    Select your vote:
-                </label>
+                <label htmlFor="vote">Select your vote:</label>
                 <select
                     name="vote"
                     className="form-select"
@@ -66,13 +79,14 @@ function ReviewForm() {
                 </select>
             </div>
 
-            <button
-                type="submit"
-                className="btn btn-primary">
+            <button type="submit" className="btn btn-primary">
                 Submit
             </button>
+            {error && (
+                <div className="text-danger mt-2">Please fill in the form correctly.</div>
+            )}
         </form>
-    )
-};
+    );
+}
 
 export default ReviewForm;
